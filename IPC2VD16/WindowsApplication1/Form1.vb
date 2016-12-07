2bd4d998e3b637bfc628cc8bd63cd9f48c8d8ce8
Ebenja <201504530@ingenieria.usac.edu.gt>
2016-12-06 23:51:32 -0600

    Metodo 1:
    Modificacion a texto del metodo 1

--- IPC2VD16/WindowsApplication1/Form1.vb
+++ IPC2VD16/WindowsApplication1/Form1.vb
@@ -1,6 +1,6 @@
 ﻿Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Label1.Text = "Metodo 1 corregido"
+        Label1.Text = "Metodo 1 modificacion"
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
