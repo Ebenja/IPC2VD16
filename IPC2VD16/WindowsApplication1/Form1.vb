64ca75b92cd91664fe9da3dad3f38fad9f125443
Ebenja <201504530@ingenieria.usac.edu.gt>
2016-12-06 23:49:02 -0600

    Tarea
    Metodos

---
+++ IPC2VD16/WindowsApplication1/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Label1.Text = "Metodo 1 corregido"
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Label1.Text = "Metodo 2"
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Label1.Text = "Metodo 3"
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Label1.Text = "Metodo 4"
+    End Sub
+End Class
